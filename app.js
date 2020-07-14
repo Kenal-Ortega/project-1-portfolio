ec83c23f3cb4c5ad778c344cdf4d58017ab18e9a
kenalOrtega <kenalortega@gmail.com>
2020-07-14 18:43:38 -0400

    updated projects

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+//HAMBURGER FUNCTION
 const $burger = $(".burger")
 const $right1 = $(".right1")
 let show = false;
@@ -17,3 +18,51 @@ const showMenu = (event) => {
 }
 
 $burger.on("click", showMenu)
+
+
+
+//JSON
+const url = "https://spreadsheets.google.com/feeds/list/1hKH-79ODVGCyHQwK9eGJwTrgQe5k758CcZEnncL9MUI/od6/public/values?alt=json"
+
+fetch(url)
+    .then(response => response.json())
+    .then(data => {
+        //console.log("data", data)
+        console.log(data.feed.entry)
+        const projects = data.feed.entry.map(entry => {
+            return {
+                title: entry.gsx$title.$t,
+                image: entry.gsx$image.$t,
+                description: entry.gsx$description.$t,
+                url: entry.gsx$url.$t
+             }
+        })
+        //console.log(projects)
+        app(projects)
+
+})
+
+const app = (data => {
+    console.log("app is running")
+    console.log(data)
+
+    const createProjectElement = (project) => {
+        const $div = $("<div>")
+        $div.append($("<h2>").text(project.title))
+        $div.append($("<p>").text(project.description))
+        $div.append($("<img>").attr("src", project.image))
+        $div.append($("<a>").attr("href", project.url))
+        return $div
+
+    }
+
+    //$("body").append(createProjectElement(data[0]))
+
+    data.forEach(project => {
+        const $projectDiv = createProjectElement(project)
+        $("body").append($projectDiv)
+    })
+
+
+
+})
