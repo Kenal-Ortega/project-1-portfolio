b922a6a97c632db4328d4cbf8966f53f2e5d083d
kenalOrtega <kenalortega@gmail.com>
2020-07-15 14:41:52 -0400

    cleaning page up

--- app.js
+++ app.js
@@ -48,7 +48,7 @@ const app = (data => {
 
     const createProjectElement = (project) => {
         const $div = $("<div>")
-        $div.append($("<h2>").text(project.title))
+        $div.append($("<h4 class='center1'>").text(project.title))
        // $div.append($("<p>").text(project.description))
         $div.append($("<img>").attr("src", project.image))
         $div.append($("<a>").attr("href", project.url))
